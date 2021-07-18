28546c25f39af0c722c04873c0e8c05345f1f05f
moises78moura <moisesicdemoura@gmail.com>
2021-07-18 15:18:01 -0300

    commit inicial

---
+++ src/test/java/br/com/mmt/config/deliveryconfigserver/DeliveryConfigServerApplicationTests.java
@@ -0,0 +1,14 @@
+package br.com.mmt.config.deliveryconfigserver;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
