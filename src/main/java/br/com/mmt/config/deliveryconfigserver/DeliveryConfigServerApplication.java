4d16b921bd519b5a979678d9a40473d745768e66
moises78moura <moisesicdemoura@gmail.com>
2021-07-18 21:23:33 -0300

    teste de commit

--- src/main/java/br/com/mmt/config/deliveryconfigserver/DeliveryConfigServerApplication.java
+++ src/main/java/br/com/mmt/config/deliveryconfigserver/DeliveryConfigServerApplication.java
@@ -7,9 +7,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @SpringBootApplication
 @EnableConfigServer
 public class DeliveryConfigServerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DeliveryConfigServerApplication.class, args);
 	}
-
 }
